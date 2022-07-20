b702addc22c47ec3163579b65e50f91237cb81cd
batuburgu <99875456+batuburgu@users.noreply.github.com>
2022-07-20 18:26:15 +0300

    Full Code

---
+++ camera-face-eye-detection.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret,frame = cap.read() #reads the image coming from camera
+    frame = cv2.flip(frame,1) #reversing mirror effect
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # making the frame gray
+
+    #cascades which will be used for detection of face and eyes
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
+
+    faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2) #rectangle around face
+        roi_gray = gray_frame[y:y+h, x:x+w] #face in gray colour
+        roi_color = frame[y:y+h, x:x+w] # coloured face
+
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,255,0), 2) #rectangle around eyes
+
+    cv2.imshow("frame",frame)
+
+    #break if user presses 'q'
+    if(cv2.waitKey(1) == ord('q')):
+        break 
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ face_train.py
@@ -0,0 +1,44 @@
+import cv2 
+import os
+import numpy as np
+
+#cascade for detecting face
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+people = ["Ben Affleck", "Madonna", "Benedict Cumberbatch"]
+features = []
+labels = []
+
+DIR = r"C:\\Users\\User\\Documents\\VSCode\\People"
+def create_train(features,labels):
+    
+    for person in people:
+        path = os.path.join(DIR,person) # gets to the person's folder
+        label = people.index(person) # saves persons index
+
+        for img in os.listdir(path):
+            img_path = os.path.join(path,img) # gets to the image in person's folder
+            current_face =cv2.imread(img_path)
+            gray = cv2.cvtColor(current_face, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.1 , 5)
+
+            for (x,y,w,h) in faces:
+                faces_roi = gray[y:y+h, x:x+w]
+                features.append(faces_roi) # appends the name to features for training it later
+                labels.append(label) # appends the label to labels for training it later
+
+    
+create_train(features,labels)
+
+features = np.array(features, dtype=object)
+labels = np.array(labels)
+
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+face_recognizer.train(features,labels)
+face_recognizer.save("training.yml")
+
+
+print("Done--------------")
+
+
+        
