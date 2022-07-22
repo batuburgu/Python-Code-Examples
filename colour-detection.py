8459350a1c294939780144136ce1273fab8fe4ff
batuburgu <99875456+batuburgu@users.noreply.github.com>
2022-07-22 20:58:18 +0300

    Full Code (Commented)

--- colour-detection.py
+++ colour-detection.py
@@ -1,34 +1,43 @@
 import cv2
 import numpy as np
 
+# will be used for printing both pictures side by side
+bigger_picture = cv2.imread("C:\\Users\\Batu\\Desktop\\gulresmi.jpg",cv2.IMREAD_COLOR) 
 
-bigger_picture = cv2.imread("C:\\Users\\Batu\\Desktop\\gulresmi.jpg",cv2.IMREAD_COLOR) # will be used for printing the other images
-sea_img = cv2.imread("C:\\Users\\Batu\\Desktop\\plajresmi.jpg", cv2.IMREAD_COLOR) # takes the image as a variable
-rose_img = cv2.imread("C:\\Users\\Batu\\Desktop\\gulresmi.jpg", cv2.IMREAD_COLOR)# takes the image as a variable
+# takes the image as a variable
+sea_img = cv2.imread("C:\\Users\\Batu\\Desktop\\plajresmi.jpg", cv2.IMREAD_COLOR) 
+rose_img = cv2.imread("C:\\Users\\Batu\\Desktop\\gulresmi.jpg", cv2.IMREAD_COLOR)
 
-lower_blue = np.array([50,50,50]) # np array for lower blue
-upper_blue = np.array([255,255,255]) # np array for upper blue
-lower_red = np.array([155,25,0]) # np array for lower red
-upper_red = np.array([179,255,255]) # np array for upper red
+# np arrays for upper and lower blue & red
+lower_blue = np.array([50,50,50]) 
+upper_blue = np.array([255,255,255]) 
+lower_red = np.array([155,25,0]) 
+upper_red = np.array([179,255,255]) 
 
-blue_hsv = cv2.cvtColor(sea_img,cv2.COLOR_BGR2HSV) #makes the image HSV
-red_hsv = cv2.cvtColor(rose_img,cv2.COLOR_BGR2HSV) #makes the imgae HSV
+# makes both images HSV
+blue_hsv = cv2.cvtColor(sea_img,cv2.COLOR_BGR2HSV) 
+red_hsv = cv2.cvtColor(rose_img,cv2.COLOR_BGR2HSV) 
 
-blue_mask = cv2.inRange(blue_hsv,lower_blue,upper_blue) # the filter which will be used for filtering the picture for tones of blue
-red_mask = cv2.inRange(red_hsv,lower_red,upper_red) # the filter which will be used for filtering the picture for tones of red
+#the filters which will be used for filtering the pictures for wanted colour tones
+blue_mask = cv2.inRange(blue_hsv,lower_blue,upper_blue) 
+red_mask = cv2.inRange(red_hsv,lower_red,upper_red) 
 
+#gets the filtered results
 red_results = cv2.bitwise_and(rose_img, rose_img, mask= red_mask)
-blue_results = cv2.bitwise_and(sea_img,sea_img, mask = blue_mask) # gets the filtered results
+blue_results = cv2.bitwise_and(sea_img,sea_img, mask = blue_mask) 
 
-sea_img = blue_results.copy() # copies the filtered version of the image to the old one
-rose_img = red_results.copy() #copies the filtered version of the image to the old one
+# changes both old images with the new filtered ones
+sea_img = blue_results.copy() 
+rose_img = red_results.copy() 
 
-resized_sea_img = cv2.resize(sea_img, (0,0), fx=0.5, fy=1) # decreases x axis value to half 
-resized_rose_img = cv2.resize(rose_img, (0,0), fx=0.5, fy=1) # decreases x axis value to half
+# halves x axis value in both pictures
+resized_sea_img = cv2.resize(sea_img, (0,0), fx=0.5, fy=1) 
+resized_rose_img = cv2.resize(rose_img, (0,0), fx=0.5, fy=1) 
 
-bigger_picture[:667,:500] = resized_sea_img # sets the left side of the picture as sea_img
-bigger_picture[:667,500:] = resized_rose_img # sets the right side of the picture as rose_img
+#adjustes the right places for both pictures(left and right)
+bigger_picture[:667,:500] = resized_sea_img 
+bigger_picture[:667,500:] = resized_rose_img 
 
-cv2.imshow("results",bigger_picture)
+cv2.imshow("results",bigger_picture) 
 cv2.waitKey(0) 
 cv2.destroyAllWindows()
